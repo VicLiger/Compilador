de54f27b3cbe4f69f6c217a83668dfa9c6237ed7
Victor Liger <victor.liger0800@gmail.com>
2026-06-05 13:29:51 -0300

    ✨ feat: criando novas funções dentro do parser

--- Core/Parser.py
+++ Core/Parser.py
@@ -1,3 +1,6 @@
+from Core.Node import NumeroNode, OperacaoBinariaNode
+
+
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
@@ -8,7 +11,41 @@ class Parser:
     def avancar(self):
         self.indice += 1
 
-        if self.indice == len(self.tokens):
+        if self.indice < len(self.tokens):
             self.token_atual = self.tokens[self.indice]
+        else:
+            self.token_atual = None
 
-        return self.token_atual
+    def processar_numero(self):
+        token = self.token_atual
+
+        if token.tipo in ["INTEIRO", "DECIMAL"]:
+            self.avancar()
+            return NumeroNode(token)
+
+
+    def processar_operacao(self, funcao, operadores):
+        esquerda = funcao()
+
+        while self.token_atual is not None and self.token_atual.tipo in operadores:
+
+            operador = self.token_atual
+            self.avancar()
+            direita = funcao()
+
+            esquerda = OperacaoBinariaNode(esquerda, operador, direita)
+
+        return esquerda
+
+
+    def processar_multiplicacao_divisao(self):
+        return self.processar_operacao(
+            self.processar_numero,
+            ["MULTIPLICACAO", "DIVISAO"]
+        )
+
+    def processar_expressao(self):
+        return self.processar_operacao(
+            self.processar_multiplicacao_divisao,
+            ["SOMA", "SUBTRACAO"]
+        )
