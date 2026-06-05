109eb486afc94bdfc66c7a9dfb5249077ab3a7a8
Victor Liger <victor.liger0800@gmail.com>
2026-06-05 12:42:10 -0300

    ✨ feat: adicionando classe node

---
+++ Core/Node.py
@@ -0,0 +1,10 @@
+class NumeroNode:
+    def __init__(self, token):
+        self.token = token
+
+
+class OperacaoBinariaNode:
+    def __init__(self, esquerda, operador, direita):
+        self.esquerda = esquerda
+        self.operador = operador
+        self.direita = direita
